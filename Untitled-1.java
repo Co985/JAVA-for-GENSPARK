a95d6880e5ae22de2af6bbd7508bab5b7734d5d5
Co985 <82610576+Co985@users.noreply.github.com>
2022-06-22 06:14:21 -0500

    Add files via upload

---
+++ Untitled-1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner; //Import the Scanner
+
+
+
+
+public class main
+{
+    public static void main (String[] args) {
+        Scanner userchoice = new Scanner(System.in);
+        System.out.println("Which cave will you go into?");
+
+
+
+    }
+}
